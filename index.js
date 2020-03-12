2c0f2b97609d735e08be0e15626d1210a5e6c1ac
Inspiron 3567 <huy.ln@classfunc.com>
2020-03-12 22:10:52 +0700

    đasadsasadsads

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  */
 
 import {AppRegistry} from 'react-native';
+import React, {setGlobal} from 'reactn';
 import App from './App';
 import {name as appName} from './app.json';
-
+setGlobal({
+    lap: [],
+});
 AppRegistry.registerComponent(appName, () => App);
